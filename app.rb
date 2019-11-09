fb6cb5e378fdef36b860ac3d5bf173137fe5d834
Pretz <38999992+Pretz0518@users.noreply.github.com>
2019-11-09 18:58:05 +0900

    complite

--- app.rb
+++ app.rb
@@ -5,6 +5,36 @@ require 'bundler/setup'
 Bundler.require
 require 'sinatra/reloader' if development?
 
+require 'sinatra/activerecord'
+
+enable :sessions
+
+helpers do
+  def current_user
+    User.find_by(id: session[:user])
+  end
+end
+
+get '/login' do
+    @logined = params[:failed]
+
+  erb :login
+end
+
+get "/password" do
+ if  params[:password]=="AGB" then
+  redirect '/'
+else
+  redirect '/login?failed=1'
+ end
+end
+
+
+get'/signout' do
+  session[:user] = nil
+  redirect'/'
+end
+
 get '/' do
   session = GoogleDrive::Session.from_config('config.json')
   session.files.each { |file| p file }
